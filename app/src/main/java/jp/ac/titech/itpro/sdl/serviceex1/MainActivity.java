3ef48c57e10184abbd922a7b7673bdd7bd8cc9fd
matsu-ITSP <matsunaga.t.ad@m.titech.ac.jp>
2018-06-01 15:41:00 +0900

    "service3のボタンができた"

--- app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
@@ -25,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         testService2();
     }
 
+    public void pressTest3(View v) {
+        testService3();
+    }
+
     private void testService1() {
         Log.d(TAG, "testService1 in " + Thread.currentThread());
         Intent intent = new Intent(this, TestService1.class);
@@ -36,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "testService2 in " + Thread.currentThread());
         Intent intent = new Intent(this, TestService2.class);
         intent.putExtra(TestService2.EXTRA_MYARG, "Hello, Service2");
+        startService(intent);
+    }
+    private void testService3() {
+        Log.d(TAG, "testService3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, TestService3.class);
+        intent.putExtra(TestService3.EXTRA_MYARG, "Hello, Service3");
         startService(intent);
     }
 
